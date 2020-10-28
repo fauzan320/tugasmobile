c02274a663c4785db10d1dce5a18958e81c1fac6
Fauzan 320 <solihulfauzan@gmail.com>
2020-10-28 10:58:13 +0700

    tambah style appbar

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,32 @@ class MyApp extends StatelessWidget { //class myapp turunan dari stlwidget
   @override 
   // penggunaan nama class yang sama
   Widget build(BuildContext context) { // metod build dari stl widget , metod akan mengembalikan sebuah widget yang merupakan tampilan dari stl widget  
+    var appBar2 = AppBar(
+          //didalanya terdapat properti titlle 
+          title:Text(
+            //memberi text pada appbar
+            'Apk AirSuDekat - SDGS 6',
+            //memberi styling appbar
+            style: TextStyle(
+              fontWeight:FontWeight.bold,
+              ),
+          ),
+          // dengan widget teks
+          
+        centerTitle: true,// membuat posisi titile ke tengah
+        backgroundColor: Colors.tealAccent[4  00],// merubah warna
+          
+        
+        );
     return MaterialApp( //mengermbalikan materialapp yang berisi materialapp
       home: Scaffold(
         //properti appbar yang  memiliki widget appbar
-        appBar: AppBar(
-          //didalanya terdapat properti titlle 
-          title:Text('Apk AirSuDekat - SDGS 6'),
-          // dengan widget teks
-        
-        ),
+        appBar: appBar2,
         body:
         Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center, // membuat posisi tengah vertikal
-            children: [
+            children:  [
               Text('Moh. Sholihul Fauzan - 182410102014'),
               Text('Arif Nurul Rahman H. - 182410102015'),
               Text('Agustian Armando - 182410102057'),
