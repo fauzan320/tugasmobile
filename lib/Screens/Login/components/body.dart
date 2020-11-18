961b7b0a82e419047ea5ce2000fad50b55a28f2f
ArifNurul2303 <humanixty@gmail.com>
2020-11-18 19:18:16 +0700

    Create body.dart

---
+++ lib/Screens/Login/components/body.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:projectawal/Screens/Login/components/background.dart';
+import 'package:projectawal/Screens/Signup/signup_screen.dart';
+import 'package:projectawal/components/already_have_an_account_acheck.dart';
+import 'package:projectawal/components/rounded_button.dart';
+import 'package:projectawal/components/rounded_input_field.dart';
+import 'package:projectawal/components/rounded_password_field.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:projectawal/Screens/dashboard.dart';
+
+
+
+class Body extends StatelessWidget {
+  void _tampilkanalert(context) {
+    AlertDialog alertDialog = new AlertDialog(
+      content: new Container(
+        height: 200.0,
+        child: new Center(
+          child: new Text("Email atau Password Yang Anda Masukan Salah"),
+        ),
+      ),
+      actions: [
+        FlatButton(
+          child: Text('Tutup'),
+          onPressed: () {
+            Navigator.of(context);
+          },
+        ),
+      ],
+    );
+    showDialog(
+      context: context,
+      child: alertDialog,
+      barrierDismissible: false,
+    );
+  }
+  const Body({
+    Key key,
+  }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+      String email,password;
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "LOGIN",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.03),
+            SvgPicture.asset(
+              "assets/icons/login.svg",
+              height: size.height * 0.35,
+            ),
+            SizedBox(height: size.height * 0.03),
+            RoundedInputField(
+              hintText: "Your Email",
+              onChanged: (e) {
+                email= e;
+              },
+            ),
+            RoundedPasswordField(
+              onChanged: (p) {
+                password = p;
+              },
+            ),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {
+                if(email=='sudekat' && password =='air') {
+                   Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return dashboard();
+                    },
+                  ),
+                );
+                }else{
+                  return _tampilkanalert(context);
+                }
+                
+              },
+
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
